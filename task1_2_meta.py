58275851ea7f64d1f1866bfaff459363dc9f6d12
Dmitriy Smirnov <dreps350@gmail.com>
2019-03-26 22:39:26 +0300

    Hot fix

--- task1_2_meta.py
+++ task1_2_meta.py
@@ -2,14 +2,14 @@ class MetaCollection(type):
 
     def __new__(cls, clsname, bases, dct):
         dct["COLLECTION"] = clsname
-        return super(MetaCollection, cls).__new__(cls, clsname, bases, dct)
+        return super().__new__(cls, clsname, bases, dct)
 
 
 class Account(metaclass=MetaCollection):
 
     def __init__(self, number: str):
-
-        assert len(number) == 13
+        if len(number) != 13:
+            raise ValueError("The length of number must be equal to 13.")
         self.number = number
 
 
